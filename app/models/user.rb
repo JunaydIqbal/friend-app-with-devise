d99061da20a0f3768e043d284ddaa96c401d5bf0
thejunaidiqbal1 <thejunaidiqbal@outlook.com>
2021-07-14 13:14:46 +0500

    login with username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,32 @@
 class User < ApplicationRecord
+
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :authentication_keys => {email: true, login: false}
+         :recoverable, :rememberable, :validatable
   
-  attr_writer :login
 
-  def login
-    @login || self.username || self.email
+  
+  def self.find_for_database_authentication warden_condition
+    conditions = warden_condition.dup
+    login1 = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login1.strip.downcase}]).first
   end
+  # attr_writer :login
 
-  validate :validate_username
+  # def login
+  #   @login || self.username || self.email
+  # end
 
-  def validate_username
-    if User.where(email: username).exists?
-      errors.add(:username, :invalid)
-    end
-  end
+  # validate :validate_username
+
+  # def validate_username
+  #   if User.where(email: username).exists?
+  #     errors.add(:username, :invalid)
+  #   end
+  # end
   
 
 end
